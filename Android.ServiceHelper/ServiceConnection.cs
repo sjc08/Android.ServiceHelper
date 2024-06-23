d57aea4be5704b23a9cd1259d736e873f43a0a96
AlexSJC <cn_sjc@outlook.com>
2024-06-23 12:41:27 +0800

    update

--- Android.ServiceHelper/ServiceConnection.cs
+++ Android.ServiceHelper/ServiceConnection.cs
@@ -5,23 +5,31 @@ namespace Asjc.Android.ServiceHelper
 {
     public class ServiceConnection<T> : Java.Lang.Object, IServiceConnection where T : Service
     {
+        private readonly ManualResetEventSlim mres = new();
+
         public Binder<T>? Binder { get; private set; }
+
+        public bool Connected => Binder != null;
+
         public T? Service => Binder?.Service;
 
-        public event Action<ComponentName?, Binder<T>?>? Connected;
-        public event Action<ComponentName?>? Disconnected;
+        public event Action<ComponentName?, Binder<T>?>? ServiceConnected;
+        public event Action<ComponentName?>? ServiceDisconnected;
+
+        public void WaitUntilConnected() => mres.Wait();
 
         public void OnServiceConnected(ComponentName? name, IBinder? service)
         {
-            var binder = service as Binder<T>;
-            Binder = binder;
-            Connected?.Invoke(name, binder);
+            Binder = (Binder<T>?)service;
+            mres.Set();
+            ServiceConnected?.Invoke(name, Binder);
         }
 
         public void OnServiceDisconnected(ComponentName? name)
         {
             Binder = null;
-            Disconnected?.Invoke(name);
+            mres.Reset();
+            ServiceDisconnected?.Invoke(name);
         }
     }
 }
