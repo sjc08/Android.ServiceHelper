4ade61bc282aee6e5c380508250b1c7bb5511322
AlexSJC <cn_sjc@outlook.com>
2024-06-23 23:03:28 +0800

    add comments

--- Android.ServiceHelper/ServiceBinder.cs
+++ Android.ServiceHelper/ServiceBinder.cs
@@ -4,6 +4,9 @@ namespace Asjc.Android.ServiceHelper
 {
     public class ServiceBinder<T>(T service) : Binder where T : Service
     {
+        /// <summary>
+        /// The bound service.
+        /// </summary>
         public T Service { get; } = service;
     }
 }
--- Android.ServiceHelper/ServiceConnector.cs
+++ Android.ServiceHelper/ServiceConnector.cs
@@ -5,15 +5,40 @@ namespace Asjc.Android.ServiceHelper
 {
     public class ServiceConnector<T> : Java.Lang.Object, IServiceConnection where T : Service
     {
+        /// <summary>
+        /// The binder associated with the connected service.
+        /// </summary>
         public ServiceBinder<T>? Binder { get; private set; }
 
+        /// <summary>
+        /// Indicates whether the connection is valid.
+        /// </summary>
         public bool Connected => Binder != null;
 
+        /// <summary>
+        /// The bound service.
+        /// </summary>
         public T? Service => Binder?.Service;
 
+        /// <summary>
+        /// Occurs when the service is connected.
+        /// </summary>
         public event Action<ComponentName?, ServiceBinder<T>?>? ServiceConnected;
+
+        /// <summary>
+        /// Occurs when the service is disconnected.
+        /// </summary>
         public event Action<ComponentName?>? ServiceDisconnected;
 
+        /// <summary>
+        /// Executes an action when the service is connected.
+        /// </summary>
+        /// <remarks>
+        /// If the service is already connected, the action is executed immediately.
+        /// If the service is not yet connected, the action is executed once the service is connected.
+        /// Note: Whether the service is connected is determined by the <see cref="Connected"/> property.
+        /// </remarks>
+        /// <param name="action">The action to execute.</param>
         public void WhenConnected(Action<T?> action)
         {
             if (Connected)
@@ -24,8 +49,11 @@ namespace Asjc.Android.ServiceHelper
             {
                 void Handler(ComponentName? name, ServiceBinder<T>? binder)
                 {
-                    ServiceConnected -= Handler;
-                    action(Service);
+                    if (Connected)
+                    {
+                        ServiceConnected -= Handler;
+                        action(Service);
+                    }
                 }
                 ServiceConnected += Handler;
             }
